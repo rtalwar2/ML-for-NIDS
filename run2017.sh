620a8d2f480ce8c1e562ffa6495e916f931f13b4
rtalwar <raman.talwar@ugent.be>
2023-05-18 00:07:10 +0200

    cleaning

--- run2017.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-echo "Hi there from GPULAb"
-pip install fastai
-pip install pyarrow
-
-python /project_ghent/raman/cic-ids-2017-images-generator.py
--- run2018.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-echo "Hi there from GPULAb"
-pip install fastai
-pip install pyarrow
-
-python /project_ghent/raman/cic-ids-2018-images-generator.py
